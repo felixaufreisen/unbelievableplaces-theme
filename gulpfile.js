b11fe9e1c24bfe37c773e48bacdb48b32b5bc900
Felix Friebe <felix-friebe@gmx.de>
2019-01-14 12:23:08 +0300

    Anpassung der Taskdefinition an Gulp 4.0. Uglify JS außer d3

--- gulpfile.js
+++ gulpfile.js
@@ -10,31 +10,32 @@ var plumber = require('gulp-plumber'),
     autoprefixer = require ('gulp-autoprefixer'),
     cleanCSS = require ('gulp-clean-css')
 
-gulp.task('css', function() {
+function css() {
   return gulp.src([
       'sass/style.scss'
     ])
     .pipe(plumber())
     .pipe(sass())
     .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
+      browsers: ['last 3 versions'],
       cascade: false
     }))
     .pipe(cleanCSS())
-    .pipe(gulp.dest('./'))
-});
+    .pipe(gulp.dest('./'));
+}
 
-gulp.task('js', function() {
+function js_uglify() {
   return gulp.src([
       'node_modules/jquery/dist/jquery.slim.js',
       'node_modules/popper.js/dist/umd/popper.js',
       'node_modules/bootstrap/dist/js/bootstrap.js',
-      'node_modules/d3/d3.min.js', // benötigt für datamaps
+      // 'node_modules/d3/d3.min.js', // benötigt für datamaps
       'node_modules/topojson/build/topojson.js', // benötigt für datamaps
       'node_modules/datamaps/dist/datamaps.world.js',
       'node_modules/@google/markerclusterer/src/markerclusterer.js',
-      'node_modules/@fortawesome/fontawesome-free/js/all.js',
-      'js/map.js',
+      'node_modules/@fortawesome/fontawesome-free/js/fontawesome.js',
+      'node_modules/@fortawesome/fontawesome-free/js/brands.js',
+      // 'node_modules/@fortawesome/fontawesome-free/js/solid.js', // Symbole manuell eingefügt, um Dateigröße zu reduzieren
       'js/*.js',
       '!js/customizer.js',
       '!js/infobox.js', // Muss nach Google-API eingebunden werden
@@ -42,14 +43,37 @@ gulp.task('js', function() {
     ])
     .pipe(plumber())
     .pipe(jshint())
+    .pipe(concat('temp.min.js'))
+    .pipe(uglify())  //d3 funktiniert dann nicht mehr
+    .pipe(gulp.dest('./js/'));
+}
+
+// d3 schmeißt nach uglify einen Fehler. Deshalb wird es ohne uglify eingebunden
+function js_add_d3() {
+  return gulp.src([
+      'node_modules/d3/d3.min.js', // benötigt für datamaps
+      'js/temp.min.js'
+    ])
+    .pipe(plumber())
+    .pipe(jshint())
     .pipe(concat('script.min.js'))
-    // .pipe(uglify())
-    .pipe(gulp.dest('./js/'))
-});
+    .pipe(gulp.dest('./js/'));
+}
 
-gulp.task('build', gulp.parallel( 'css', 'js' ));
+// Gebündelte Aufgaben definieren
+var js = gulp.series( js_uglify, js_add_d3 );
+var build = gulp.parallel( css, js );
 
-gulp.task('watch', function() {
-  gulp.watch('sass/**/*.scss', gulp.series( 'css' ))
-  gulp.watch(['js/*.js', '!js/customizer.js', '!js/script.min.js'], gulp.series( 'js' ))
-});
+function watch() {
+  gulp.watch('sass/**/*.scss', css)
+  gulp.watch(['js/*.js', '!js/script.min.js'], js)
+}
+
+// Tasks definieren
+gulp.task('css', css);
+
+gulp.task('js', js);
+
+gulp.task('build', build);
+
+gulp.task('watch', watch);
